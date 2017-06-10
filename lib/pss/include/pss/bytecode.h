7573cc73d79302661ab6848fb7d8fdc0d654130d
Hao Hou <ghost89413@gmail.com>
2017-06-10 07:47:05 -0400

    update the bytecode header

--- lib/pss/include/pss/bytecode.h
+++ lib/pss/include/pss/bytecode.h
@@ -12,6 +12,21 @@
 #define __PSS_BYTECODE_H__
 
 /**
+ * @brief The identifier of a segment in a bytecode table
+ **/
+typedef uint32_t pss_bytecode_segid_t;
+
+/**
+ * @brief The address which is used to addressing one instruction inside a code segment
+ **/
+typedef uint32_t pss_bytecode_addr_t;
+
+/**
+ * @brief The in-segment label id
+ **/
+typedef uint32_t pss_bytecode_label_t;
+
+/**
  * @brief The code used to identify what kinds of operation it is 
  **/
 typedef enum {
@@ -172,7 +187,7 @@ int pss_bytecode_table_free(pss_bytecode_table_t* table);
  * @param segment the segment to add
  * @return The segment id, or error code
  **/
-int pss_bytecode_table_append(pss_bytecode_table_t* table, const pss_bytecode_segment_t* segment);
+pss_bytecode_segid_t pss_bytecode_table_append(pss_bytecode_table_t* table, const pss_bytecode_segment_t* segment);
 
 /**
  * @brief Get the segment from the id
@@ -180,14 +195,14 @@ int pss_bytecode_table_append(pss_bytecode_table_t* table, const pss_bytecode_se
  * @param id The id we want to query
  * @return the segment or NULL on error
  **/
-const pss_bytecode_segment_t* pss_bytecode_table_get(const pss_bytecode_table_t* table, int id);
+const pss_bytecode_segment_t* pss_bytecode_table_get(const pss_bytecode_table_t* table, pss_bytecode_segid_t id);
 
 /**
  * @brief Get the segment id of the entry point of this bytecode table
  * @param table The bytecode table 
  * @return The segment ID for the entry point or error code
  **/
-int pss_bytecode_table_get_entry_point(const pss_bytecode_table_t* table);
+pss_bytecode_segid_t pss_bytecode_table_get_entry_point(const pss_bytecode_table_t* table);
 
 /**
  * @brief Set the entry point segment id of the given bytecode table
@@ -195,7 +210,7 @@ int pss_bytecode_table_get_entry_point(const pss_bytecode_table_t* table);
  * @param id the entry point segment id
  * @return status code
  **/
-int pss_bytecode_table_set_entry_point(pss_bytecode_table_t* table, int id);
+int pss_bytecode_table_set_entry_point(pss_bytecode_table_t* table, pss_bytecode_segid_t  id);
 
 /**
  * @brief Create a new bytecode segment
@@ -211,15 +226,42 @@ pss_bytecode_segment_t* pss_bytecode_segment_new();
 int pss_bytecode_segment_free(pss_bytecode_segment_t* segment);
 
 /**
+ * @brief Allocate a new label in the bytecode table
+ * @note  This function is used in code generation, because sometimes, we knows we should
+ *        have a jump instruction at this point, but the target address is still unknown.
+ *        So this makes us need to use a placeholder for this address and patch it later
+ * @param segment The code segment we want to define the label in
+ * @return The label id or error code
+ **/
+pss_bytecode_label_t pss_bytecode_segment_label_alloc(pss_bytecode_segment_t* segment);
+
+/**
+ * @brief Patch the label with the given address
+ * @note This function is a part of the label mechanism, when the code generator do not
+ *       know the jump target, it allocates a label and move on. Whenever the code gnereator
+ *       has generated the target instruction, which means the address is known for now, we
+ *       use this function to patch the bytecode
+ * @param segment The code segment we want to patch
+ * @param label   The label we want to patch
+ * @param addr    The address value we want to patch
+ * @return status code
+ **/
+int pss_bytecode_segment_patch_label(pss_bytecode_segment_t* segment, pss_bytecode_label_t label, pss_bytecode_addr_t addr);
+
+/**
  * @brief Append a new instruction to the segment
  * @param segment The segment we want append the instruction to
  * @param opcode The opcode we want to append
- * @param num    The numeric constant for this instruction
- * @param str    The string constant for this instruction
+ * @param num    The numeric constant for this instruction, if the instruction do not requires an constant, it will be ignored
+ * @param str    The string constant for this instruction, if the instruction do not requires an string const, it will be ignored
+ * @param label  The label we want to use as an numeric constant placeholder 
  * @note  the varargs is the register list, end with ERROR_CODE(int)
- * @return status code
+ * @return The address of the newly appended instruction
  **/
-int pss_bytecode_segment_append_inst(pss_bytecode_segment_t* segment, pss_bytecode_opcode_t opcode, int num, const char* str, ...);
+pss_bytecode_addr_t  pss_bytecode_segment_append_inst(pss_bytecode_segment_t* segment, 
+		                                              pss_bytecode_opcode_t opcode, 
+		                                              int num, pss_bytecode_label_t label, const char* str, 
+													  ...) __attribute__((sentinel));
 
 /**
  * @brief Get the instruction information header at given address in the bytecode segment
@@ -231,7 +273,8 @@ int pss_bytecode_segment_append_inst(pss_bytecode_segment_t* segment, pss_byteco
  * @param regbuf The buffer used to return the register list
  * @return status code
  **/
-int pss_bytecode_segment_get_inst(const pss_bytecode_segment_t* segment, uint32_t addr, pss_bytecode_info_t* infobuf, int* numbuf, char const** strbuf, int const ** regbuf);
+int pss_bytecode_segment_get_inst(const pss_bytecode_segment_t* segment, pss_bytecode_addr_t addr, 
+		                          pss_bytecode_info_t* infobuf, int* numbuf, char const** strbuf, int const ** regbuf);
 
 /**
  * @brief Get the human-readable representation of the instruction at the address addr in the segment
@@ -241,6 +284,6 @@ int pss_bytecode_segment_get_inst(const pss_bytecode_segment_t* segment, uint32_
  * @param sz      The string buffer size
  * @return        The result string representation of the instruction
  **/
-const char* pss_bytecode_segment_inst_str(const pss_bytecode_segment_t* segment, uint32_t addr, char* buf, size_t sz);
+const char* pss_bytecode_segment_inst_str(const pss_bytecode_segment_t* segment, pss_bytecode_addr_t addr, char* buf, size_t sz);
 
 #endif /* __PSS_BYTECODE_H__ */
