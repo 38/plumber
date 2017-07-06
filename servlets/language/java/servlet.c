58f615f067e418ff28e3ffd514e9d332b18951ea
Hao Hou <ghost89413@gmail.com>
2017-07-05 21:50:26 -0400

    Start working on Java servlet

--- servlets/language/java/servlet.c
+++ servlets/language/java/servlet.c
@@ -1,3 +1,8 @@
 #include <pservlet.h>
 
+//useful links: 
+// Call C function from Java: http://web.archive.org/web/20120419230023/http://java.sun.com/docs/books/jni/html/start.html 
+// Call Java from C: http://docs.oracle.com/javase/1.5.0/docs/guide/jni/spec/invocation.html
+// AttachCurrentThread 
+
 
