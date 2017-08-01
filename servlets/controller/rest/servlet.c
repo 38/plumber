35be4456a98347ea16928ca9257d6ea2e4e02a07
Hao Hou <ghost89413@gmail.com>
2017-07-31 22:26:42 -0600

    Add servlet code

---
+++ servlets/controller/rest/servlet.c
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2017, Hao Hou
+ **/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <pservlet.h>
+#include <pstd.h>
+
+/**
+ * @brief The context for a rest storage command
+ **/
+typedef struct {
+	pipe_t                 output;          /*!< The input pipe */
+	pstd_type_accessor_t   opcode_acc;      /*!< The opcode accessor */
+	pstd_type_accessor_t   object_id_acc;   /*!< The object ID accessor */
+	pstd_type_accessor_t   parent_id_acc;   /*!< The parent ID accessor */
+	pstd_type_accessor_t   content_acc;     /*!< The content accessor */
+	pstd_type_accessor_t   param_acc;       /*!< The parameter accessor */
+} command_ctx_t;
+
+/**he         
+ * @param The servlet context
+ **/
+typedef struct {
+	pipe_t                 request;         /*!< The request used to analyze */
+	command_ctx_t*         commands;        /*!< The storage command to perfome */
+	uint32_t count;                         /*!< The number of outputs */
+	struct {
+		uint32_t           CREATE;          /*!< Create a resource */
+		uint32_t           DELETE;          /*!< Delete a resource */
+		uint32_t           QUERY;           /*!< Query a resource */
+		uint32_t           MODIFY;          /*!< Modify a resource */
+		uint32_t           CONTENT;         /*!< Get the content of a source */
+	}                      opcode;          /*!< The operation code defined by the protocol */
+	struct {
+		uint32_t           GET;             /*!< GET HTTP request */
+		uint32_t           POST;            /*!< POST HTTP request */
+		uint32_t           DELETE;          /*!< DELETE HTTP request */
+	}                      method_code;     /*!< The method code */
+	pstd_type_model_t*     model;           /*!< The type model */
+	pstd_type_accessor_t   method_acc;      /*!< The method accessor */
+	pstd_type_accessor_t   path_acc;        /*!< The path accessor */
+	pstd_type_accessor_t   param_acc;       /*!< The parameter accessor */
+	pstd_type_accessor_t   data_acc;        /*!< The data accessor */
+} context_t; 
+
+SERVLET_DEF = {
+	.desc    = "RESTful API Controller",
+	.version = 0x0,
+	.size    = sizeof(context_t)
+};
