bb94bcb4a4f9b4d6bcd10e58837a9781cf69e8bc
Hao Hou <ghost89413@gmail.com>
2017-08-09 16:05:19 -0600

    Parse the event

--- src/module/simulate/module.c
+++ src/module/simulate/module.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 #include <error.h>
 #include <utils/log.h>
@@ -21,15 +22,18 @@ typedef struct _event_t {
 	char*                 label;  /*!< The label for this event t */
 	size_t                size;   /*!< The size of this event */
 	char*                 data;   /*!< The data for this event */
-	struct _input_data_t* next;   /*!< The next event in the event list */
+	struct _event_t*      next;   /*!< The next event in the event list */
+	uint32_t              terminate:1; /*!< Indicates this event is actuall terminate the platform */
 } _event_t;
 
 /**
  * @brief The module context 
  **/
 typedef struct {
-	_event_t*      event_list;  /*!< The event we want to simulate */
-	FILE*          outfile;     /*!< The file we want to dump the output to */
+	_event_t*      event_list_tail;  /*!< The tail of the event list */
+	_event_t*      event_list_head;  /*!< The event we want to simulate */
+	char*          label;            /*!< The label for this module instance */
+	FILE*          outfile;          /*!< The file we want to dump the output to */
 } _module_context_t;
 
 /**
@@ -58,6 +62,17 @@ static inline int _parse_comment(FILE* fp)
 	return 0;
 }
 
+static inline void _append_event(_module_context_t* ctx, _event_t* event)
+{
+	if(ctx->event_list_head == NULL) ctx->event_list_head = ctx->event_list_tail = event;
+	else
+	{
+		ctx->event_list_tail->next = event;
+		event->next = NULL;
+		ctx->event_list_tail = event;
+	}
+}
+
 static inline int _parse_command(_module_context_t* ctx, FILE* fp)
 {
 	(void) ctx; /* TODO: remove this */
@@ -68,19 +83,36 @@ static inline int _parse_command(_module_context_t* ctx, FILE* fp)
 	char const* label = NULL;
 	
 	char* ptr;
-	for(ptr = buf; *ptr == 0 || *ptr == ' ' || *ptr == '\t'; ptr ++);
+	for(ptr = buf; !(*ptr == 0 || *ptr == ' ' || *ptr == '\t' || *ptr == '\r' || *ptr == '\n'); ptr ++);
 	command = buf;
 	if(*ptr != 0)
 	{
 		*ptr = 0;
 		for(ptr ++; *ptr == ' ' || *ptr == '\t'; ptr++);
 		label = ptr;
+		for(;!(*ptr == 0 || *ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == '\r'); ptr ++);
+		ptr[0] = 0;
 	}
-	if(label == NULL || label[0] == 0) 
-		ERROR_RETURN_LOG(int, "Missing event label: it should follows pattern .<command> <label>");
+
 
 	int is_text = (strcmp(command, "TEXT") == 0);
-	int is_file = (strcmp(command, "FILE") == 0);
+	int is_file = (!is_text && strcmp(command, "FILE") == 0);
+
+	if(!is_text && !is_file && strcmp(command, "STOP") == 0)
+	{
+		_event_t* event = (_event_t*)calloc(sizeof(*event), 1);
+		if(NULL == event) ERROR_LOG_ERRNO_GOTO(ERR, "Cannot allocate memory for the event");
+		event->terminate = 1;
+		_append_event(ctx, event);
+		return 0;
+	}
+	
+	if(label[0] == 0) ERROR_RETURN_LOG(int, "Missing event label");
+
+	LOG_DEBUG("Data %s event section %s", command, label);
+
+	if(label == NULL || label[0] == 0) 
+		ERROR_RETURN_LOG(int, "Missing event label: it should follows pattern .<command> <label>");
 
 	if(!is_text && !is_file) ERROR_RETURN_LOG(int, "Invalid event type %s", command);
 
@@ -103,7 +135,7 @@ static inline int _parse_command(_module_context_t* ctx, FILE* fp)
 			while(ch == ' ' || ch == '\t' || ch == '\r')
 				ch = fgetc(fp);
 			if(ch == EOF || ch == '\n') break;
-			else ERROR_LOG_GOTO(ERR, "Unexpected end of event syntax, a line with .END is expected");
+			else goto FLUSH;
 		}
 		else
 		{
@@ -112,8 +144,10 @@ static inline int _parse_command(_module_context_t* ctx, FILE* fp)
 				state ++;
 				continue;
 			}
-			size_t delta = (size_t)(state - event_end + 1);
-			size_t new_size = size;
+			size_t delta, new_size;
+FLUSH:
+			delta = (size_t)(state - event_end);
+			new_size = size;
 			while(len + delta + 1 >= new_size)
 				new_size *= 2;
 			if(new_size != size)
@@ -128,30 +162,70 @@ static inline int _parse_command(_module_context_t* ctx, FILE* fp)
 				event_data[len++] = *p;
 			event_data[len++] = (char)ch;
 			event_data[len] = 0;
+			state = event_end;
 		}
 	}
 
-	/* Then we have the content */
+	_event_t* event = (_event_t*)calloc(sizeof(*event), 1);
+	if(NULL == event) ERROR_LOG_ERRNO_GOTO(ERR, "Cannot allocate memory for the event");
+
+	if(NULL == (event->label = strdup(label)))
+		ERROR_LOG_ERRNO_GOTO(ERR, "Cannot duplicate the label name");
+
 	if(is_text)
 	{
-		/* TODO: Then we need warp this text in a event struct */
+		event->data = event_data;
+		event->size = len;
 	}
 	else if(is_file)
 	{
-		/* TODO: We need read the file content */
+		struct stat st;
+		if(stat(event_data, &st) < 0)
+			ERROR_LOG_ERRNO_GOTO(ERR, "Cannot get the stat of the file %s", event_data);
+		
+		size_t data_size = (size_t)st.st_size;
+
+		char* data_buffer = (char*)malloc(data_size);
+		FILE* data_fp = fopen(event_data, "rb");
+		if(data_fp == NULL)
+			ERROR_LOG_ERRNO_GOTO(FILE_ERR, "Cannot open the data file %s", event_data);
+
+		event->data = data_buffer;
+
+		for(;data_size > 0;)
+		{
+			size_t bytes_written = fread(data_buffer, 1, data_size, data_fp);
+			if(bytes_written == 0) 
+				ERROR_LOG_ERRNO_GOTO(FILE_ERR, "Cannot read the data from file %s", event_data);
+			data_size -= bytes_written;
+			data_buffer += bytes_written;
+		}
+		event->size = (size_t)st.st_size;
+		fclose(data_fp);
+		free(event_data);
+		goto EXIT_NORMALLY;
+FILE_ERR:
+		if(NULL != event->data) free(event->data);
+		if(NULL != data_fp) fclose(data_fp);
+		goto ERR;
 	}
 	else ERROR_LOG_ERRNO_GOTO(ERR, "Code bug!");
+
+EXIT_NORMALLY:
+	_append_event(ctx, event);
 
 	return 0;
 ERR:
 	if(NULL != event_data) free(event_data);
 	return ERROR_CODE(int);
-
 }
 
 static inline int _parse_input(_module_context_t* ctx, FILE* fp)
 {
 	(void)ctx;
+#ifdef LOG_DEBUG_ENABLED
+	uint32_t count = 0;
+#endif
 	for(;;)
 	{
 		int ch = fgetc(fp);
@@ -167,16 +241,17 @@ static inline int _parse_input(_module_context_t* ctx, FILE* fp)
 				if(ERROR_CODE(int) == _parse_comment(fp))
 					ERROR_RETURN_LOG(int, "Cannot parse the comment");
 				break;
-			case '.':  /* TODO */
-#if 0
+			case '.':
+				count ++;
 				if(ERROR_CODE(int) == _parse_command(ctx, fp))
 					ERROR_RETURN_LOG(int, "Cannot parse the command");
-#endif
 				break;
 			default:
 				ERROR_RETURN_LOG(int, "Invalid sytax");
 		}
 	}
+
+	LOG_DEBUG("%u events has been loaded from file", count);
 
 	return 0;
 }
@@ -186,24 +261,34 @@ static int _init(void* __restrict ctxbuf, uint32_t argc, char const* __restrict 
 	_module_context_t* ctx = (_module_context_t*)ctxbuf;
 	if(NULL == ctx) ERROR_RETURN_LOG(int, "Invalid arguments");
 
+	memset(ctx, 0, sizeof(*ctx));
+
 	const char input_prefix[] = "input=";
 	const char output_prefix[] = "output=";
+	const char id_prefix[] = "label=";
 
 	const char* input_name = NULL;
 	const char* output_name = NULL;
+	const char* id = NULL;
 
 	uint32_t i;
 	for(i = 0; i < argc; i ++)
 	{
 		if(_start_with(argv[i], input_prefix)) input_name = argv[i] + sizeof(input_prefix) - 1;
 		else if(_start_with(argv[i], output_prefix)) output_name = argv[i] + sizeof(output_name) - 1;
-		else ERROR_RETURN_LOG(int, "Invalid module initialization arguments, expected: simulate input=inputfile output=outputfile");
+		else if(_start_with(argv[i], id_prefix)) id = argv[i] + sizeof(output_name) - 1;
+		else ERROR_RETURN_LOG(int, "Invalid module initialization arguments, expected: simulate input=inputfile output=outputfile label=labelname");
 	}
 
 	if(input_name == NULL || input_name[0] == 0)
 		ERROR_RETURN_LOG(int, "Missing input file name");
 	if(output_name == NULL || output_name[0] == 0)
 		ERROR_RETURN_LOG(int, "Missing output file name");
+	if(id == NULL || id[0] == 0)
+		ERROR_RETURN_LOG(int, "Missing label of the module");
+
+	if(NULL == (ctx->label = strdup(id)))
+		ERROR_RETURN_LOG_ERRNO(int, "Cannot duplicate the ID label");
 
 	/* Then let's parse the input file */
 	FILE* fp = fopen(input_name, "r");
@@ -212,12 +297,42 @@ static int _init(void* __restrict ctxbuf, uint32_t argc, char const* __restrict 
 	if(ERROR_CODE(int) == _parse_input(ctx, fp))
 	{
 		fclose(fp);
-		ERROR_RETURN_LOG(int, "Cannot parse the input file %s", input_name);
+		ERROR_LOG_ERRNO_GOTO(ERR, "Cannot pasrse the input");
 	}
 
 	fclose(fp);
-	/* TODO */
+
+	if(NULL == (ctx->outfile = fopen(output_name, "wb")))
+		ERROR_LOG_ERRNO_GOTO(ERR, "Cannot open the output file %s", output_name);
+
+	LOG_DEBUG("Event Simulation Module has been initialized, input = %s, output = %s", input_name, output_name);
+
 	return 0;
+ERR:
+	return ERROR_CODE(int);
+}
+
+static inline int _cleanup(void* __restrict ctxbuf)
+{
+	_module_context_t* ctx = (_module_context_t*)ctxbuf;
+	if(ctx->label != NULL) free(ctx->label);
+	if(ctx->outfile != NULL) fclose(ctx->outfile);
+	for(;ctx->event_list_head != NULL;)
+	{
+		_event_t* event = ctx->event_list_head;
+		ctx->event_list_head = ctx->event_list_head->next;
+		if(event->data != NULL) free(event->data);
+		if(event->label != NULL) free(event->label);
+		free(event);
+	}
+	return 0;
+}
+
+static const char* _get_path(void* __restrict ctx, char* buf, size_t sz)
+{
+	_module_context_t* context = (_module_context_t*)ctx;
+	snprintf(buf, sz, "%s", context->label);
+	return buf;
 }
 
 
@@ -225,5 +340,7 @@ itc_module_t module_simulate_module_def = {
 	.mod_prefix   = "pipe.simulate",
 	.context_size = sizeof(_module_context_t),
 	.handle_size  = sizeof(_handle_t),
-	.module_init  = _init
+	.module_init  = _init,
+	.module_cleanup = _cleanup,
+	.get_path = _get_path
 };
