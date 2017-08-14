b4f12f5b75def28a72a7727c4019d40eb2d9c269
Hao Hou <ghost89413@gmail.com>
2017-08-14 15:12:20 -0600

    Add a library that handles JSON schema, because it is likely we would like to use it later in other restful servlet

---
+++ lib/jsonschema/include/jsonschema.h
@@ -0,0 +1,11 @@
+/**
+ * Copyright (C) 2017, Hao Hou
+ **/
+/**
+ * @brief The JSON schema library
+ * @file jsonschema/include/jsonschema.h
+ **/
+#ifndef __JSONSCHEMA_H__
+#define __JSONSCHEMA_H__
+
+#endif /* __JSONSCHEMA_H__ */
