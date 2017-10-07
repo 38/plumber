386af82bd1d72a0ac267818ab79449e62a136c30
Hao Hou <ghost89413@gmail.com>
2017-10-06 22:10:14 -0600

    start working on the poll object for darwin

--- src/os/darwin/event.c
+++ src/os/darwin/event.c
@@ -3,7 +3,18 @@
  **/
 #include <constants.h>
 #ifdef __DARWIN__
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+
+#include <error.h>
+
 #include <os/os.h>
-/* TODO */
-#error("Darwin support is not finished")
+#include <utils/log.h>
+
 #endif /*__DARWIN__ */
