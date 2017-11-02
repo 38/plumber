98199cdc34a312dafa5ccb59a1e0ecb6ffb8557f
Hao Hou <ghost89413@gmail.com>
2017-11-02 11:30:05 -0600

    Add function signatures

--- include/sched/daemon.h
+++ include/sched/daemon.h
@@ -20,4 +20,26 @@ int sched_daemon_init();
  **/
 int sched_daemon_finalize();
 
+/**
+ * @brief Make current application a daemon, it will do the normal daemonize,
+ *        create the controlling socket and pid file. When runtime.daemon.id == ""
+ *        then this function does nothing
+ * @return status code
+ **/
+int sched_daemon_daemonize();
+
+/**
+ * @brief The stop the daemon with deamon name
+ * @param daemon_name The name of the Deamon
+ * @return status code
+ **/
+int sched_daemon_stop(const char* daemon_name);
+
+/**
+ * @brief Get the list of current running Plumber daemon list
+ * @param ret The return array
+ * @return The size of array, error code on error cases
+ **/
+int sched_daemon_list(char ** ret);
+
 #endif /* __SCHED_DAEMON_H__ */
