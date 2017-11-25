e9da4b895d08464d011b2a6764857aadd1d9dfbc
Hao Hou <ghost89413@gmail.com>
2017-11-25 13:13:34 -0700

    Add clang flags file

---
+++ build/compiler/clang_0_0_0.cmake
@@ -0,0 +1,3 @@
+set(COMPILER_CFLAGS "-g -Wpointer-arith -Wformat=2 -Wconversion -Wextra -Wall -Werror -Wshadow -Wcast-qual -Wstrict-overflow=2 -Wuninitialized -Wmissing-prototypes -Wbad-function-cast -Wstrict-prototypes")
+set(COMPILER_CXXFLAGS "-nostdinc++ -I/usr/include/c++/v1 -g -Wpointer-arith -Wformat=2 -Wconversion -Wextra -Wall -Werror -Wshadow -Wcast-qual -Wstrict-overflow=2 -Wuninitialized")
+set(LIB_PSS_VM_STACK_LIMIT 384)
