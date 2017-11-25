2f823b72d2ed542a9393ae7b818eb05659a66cb3
Hao Hou <ghost89413@gmail.com>
2017-11-25 13:15:51 -0700

    Promote the strict overflow level to 5 for clang

--- build/compiler/clang_0_0_0.cmake
+++ build/compiler/clang_0_0_0.cmake
@@ -1,3 +1,5 @@
-set(COMPILER_CFLAGS "-g -Wpointer-arith -Wformat=2 -Wconversion -Wextra -Wall -Werror -Wshadow -Wcast-qual -Wstrict-overflow=2 -Wuninitialized -Wmissing-prototypes -Wbad-function-cast -Wstrict-prototypes")
-set(COMPILER_CXXFLAGS "-nostdinc++ -I/usr/include/c++/v1 -g -Wpointer-arith -Wformat=2 -Wconversion -Wextra -Wall -Werror -Wshadow -Wcast-qual -Wstrict-overflow=2 -Wuninitialized")
-set(LIB_PSS_VM_STACK_LIMIT 384)
+set(COMPILER_CFLAGS "-g -Wpointer-arith -Wformat=2 -Wconversion -Wextra -Wall -Werror -Wshadow -Wcast-qual -Wstrict-overflow=5 -Wuninitialized -Wmissing-prototypes -Wbad-function-cast -Wstrict-prototypes")
+set(COMPILER_CXXFLAGS "-nostdinc++ -I/usr/include/c++/v1 -g -Wpointer-arith -Wformat=2 -Wconversion -Wextra -Wall -Werror -Wshadow -Wcast-qual -Wstrict-overflow=5 -Wuninitialized")
+if("${LIB_PSS_VM_STACK_LIMIT}" STREQUAL "")
+	set(LIB_PSS_VM_STACK_LIMIT 384)
+endif("${LIB_PSS_VM_STACK_LIMIT}" STREQUAL "")
