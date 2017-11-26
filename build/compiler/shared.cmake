4256ad653802b424a09fc6964efc5d5248546eb8
Hao Hou <ghost89413@gmail.com>
2017-11-25 18:21:52 -0700

    Add new warnings

--- build/compiler/shared.cmake
+++ build/compiler/shared.cmake
@@ -1,2 +1,2 @@
-set(SHARED_CFLAGS "-g -Wpointer-arith -Wformat=2 -Wconversion -Wextra -Wall -Werror -Wshadow -Wcast-qual -Wuninitialized -Wmissing-prototypes -Wbad-function-cast -Wstrict-prototypes")
-set(SHARED_CXXFLAGS "-g -Wpointer-arith -Wformat=2 -Wconversion -Wextra -Wall -Werror -Wshadow -Wcast-qual -Wuninitialized")
+set(SHARED_CFLAGS "-g -Wpointer-arith -Wformat=2 -Wconversion -Wextra -Wall -Werror -Wshadow -Wcast-qual -Wmissing-prototypes -Wbad-function-cast -Wstrict-prototypes")
+set(SHARED_CXXFLAGS "-g -Wpointer-arith -Wformat=2 -Wconversion -Wextra -Wall -Werror -Wshadow -Wcast-qual")
