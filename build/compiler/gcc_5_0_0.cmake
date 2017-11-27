4a2612fe05a6b2adef756534a18c2feb5ca7c852
Hao Hou <ghost89413@gmail.com>
2017-11-26 22:10:57 -0700

    awful GCC bugs

--- build/compiler/gcc_5_0_0.cmake
+++ build/compiler/gcc_5_0_0.cmake
@@ -1,3 +1,3 @@
 include("${CMAKE_CURRENT_SOURCE_DIR}/build/compiler/shared.cmake")
-set(COMPILER_CFLAGS "${SHARED_CFLAGS} -Wstrict-overflow=2")
-set(COMPILER_CXXFLAGS "${SHARED_CXXFLAGS} -Wstrict-overflow=2")
+set(COMPILER_CFLAGS "${SHARED_CFLAGS} -Wstrict-overflow=1")
+set(COMPILER_CXXFLAGS "${SHARED_CXXFLAGS} -Wstrict-overflow=1")
