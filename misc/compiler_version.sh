bac37101f09d94e49049e1e7fb21c328d0e3807e
Hao Hou <ghost89413@gmail.com>
2017-11-27 23:46:23 -0700

    Detect ICC as well

--- misc/compiler_version.sh
+++ misc/compiler_version.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -eo pipefail
 echo '
-#if defined(__GNUC__) && !defined(__clang__)
+#if defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER)
 gcc#__GNUC__#__GNUC_MINOR__#__GNUC_PATCHLEVEL__
+#elif defined(__INTEL_COMPILER)
+icc#__INTEL_COMPILER#__INTEL_COMPILER#__INTEL_COMPILER_UPDATE
 #elif defined(__clang__)
 clang#__clang_major__#__clang_minor__#__clang_patchlevel__
 #else
 unknown
-#endif' |  $1 -E - | sed --silent '/\(^[^#]..*$\)/s/#/_/gp'
+#endif' |  $1 -E - | sed --silent '/\(^[^#]..*$\)/s/#/_/gp' | awk -F_ '$1 != "icc"{print} $1 == "icc" {print "icc_"$2/100"_"$3%100"_"$4}'
