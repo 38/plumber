5b0484f844a8a150b2e3a6a3a83bdaad64509c22
Hao Hou <ghost89413@gmail.com>
2018-01-19 09:53:17 -0700

    Use PCRE instead of RE2 due to memory leak

---
+++ servlets/dataflow/regex/re.c
@@ -0,0 +1,139 @@
+/**
+ * Copyright (C) 2018, Hao Hou
+ **/
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <pcre.h>
+
+#include <error.h>
+
+#include <pservlet.h>
+
+#include <re.h>
+
+/**
+ * @brief The actual data structure for the regex 
+ **/
+struct _re_t {
+	pcre*         regex;   /*!< The PCRE object */
+	pcre_extra*   extra;   /*!< The PCRE extra data */
+};
+
+re_t* re_new(const char* regex)
+{
+	if(NULL == regex) ERROR_PTR_RETURN_LOG("Invalid arguments");
+
+	re_t* ret = (re_t*)calloc(sizeof(re_t), 1);
+
+	if(NULL == ret) 
+		ERROR_PTR_RETURN_LOG_ERRNO("Cannot allocate memory for the regular expression object");
+
+	const char* pcre_err_msg = "";
+	int pcre_err_ofs = 0;
+
+
+	if(NULL == (ret->regex = pcre_compile(regex, 0, &pcre_err_msg, &pcre_err_ofs, NULL)))
+		ERROR_LOG_GOTO(ERR, "Cannot compile regular expression: %s at %d", pcre_err_msg, pcre_err_ofs);
+
+	if(NULL == (ret->extra = pcre_study(ret->regex, 0, &pcre_err_msg)))
+		ERROR_LOG_GOTO(ERR, "Cannot optimize the regular expression: %s", pcre_err_msg);
+
+	return ret;
+
+ERR:
+	if(NULL != ret->extra) 
+#ifdef PCRE_CONFIG_JIT
+		pcre_free_study(ret->extra);
+#else
+		pcre_free(ret->extra);
+#endif
+	if(NULL != ret->regex) pcre_free(ret->regex);
+
+	free(ret);
+
+	return NULL;
+}
+
+int re_free(re_t* obj)
+{
+	if(NULL == obj) ERROR_RETURN_LOG(int, "Invalid arguments");
+
+	if(obj->extra != NULL)
+#ifdef PCRE_CONFIG_JIT
+		pcre_free_study(obj->extra);
+#else
+		pcre_free(obj->extra);
+#endif
+
+	if(NULL != obj->regex) 
+		pcre_free(obj->regex);
+
+	free(obj);
+
+	return 0;
+}
+   
+#if PCRE_MAJOR >= 8 && PCRE_MINOR >= 32
+#	define PCRE_EXEC pcre_jit_exec
+#else
+#	define PCRE_EXEC pcre_exec
+#endif
+
+int re_match_full(re_t* obj, const char* text, size_t len)
+{
+	if(NULL == obj || NULL == obj->regex || NULL == text)
+		ERROR_RETURN_LOG(int, "Invalid arguments");
+
+	int ovec[30];
+
+	int match_rc = pcre_exec(obj->regex, obj->extra, text, (int)len, 0, 0, ovec, sizeof(ovec) / sizeof(ovec[0]));
+
+	switch(match_rc)
+	{
+		case PCRE_ERROR_NOMATCH:
+			return 0;
+		case PCRE_ERROR_NULL:
+			ERROR_RETURN_LOG(int, "pcre_exec: Invalid arguments");
+		case PCRE_ERROR_BADOPTION:
+			ERROR_RETURN_LOG(int, "pcre_exec: Bad options");
+		case PCRE_ERROR_BADMAGIC:
+			ERROR_RETURN_LOG(int, "pcre_exec: Invalid regex object");
+		case PCRE_ERROR_NOMEMORY:
+			ERROR_RETURN_LOG(int, "pcre_exec: Out of meomry");
+		default:
+			ERROR_RETURN_LOG(int, "pcre_exec: generic error");
+	}
+}
+
+int re_match_partial(re_t* obj, const char* text, size_t len)
+{
+	if(NULL == obj || NULL == obj->regex || NULL == text)
+		ERROR_RETURN_LOG(int, "Invalid arguments");
+	
+	int ovec[30];
+
+	int match_rc = pcre_exec(obj->regex, obj->extra, text, (int)len, 0, PCRE_PARTIAL_SOFT, ovec, sizeof(ovec) / sizeof(ovec[0]));
+
+	switch(match_rc)
+	{
+		case PCRE_ERROR_NOMATCH:
+			return 0;
+		case PCRE_ERROR_NULL:
+			ERROR_RETURN_LOG(int, "pcre_exec: Invalid arguments");
+		case PCRE_ERROR_BADOPTION:
+			ERROR_RETURN_LOG(int, "pcre_exec: Bad options");
+		case PCRE_ERROR_BADMAGIC:
+			ERROR_RETURN_LOG(int, "pcre_exec: Invalid regex object");
+		case PCRE_ERROR_NOMEMORY:
+			ERROR_RETURN_LOG(int, "pcre_exec: Out of meomry");
+		default:
+			ERROR_RETURN_LOG(int, "pcre_exec: generic error");
+	}
+
+	return 0;
+}
+
--- servlets/dataflow/regex/servlet.c
+++ servlets/dataflow/regex/servlet.c
@@ -5,7 +5,160 @@
 #include <proto.h>
 #include <pstd.h>
 
+#include <re.h>
+
+/**
+ * @brief The servlet context 
+ **/
+typedef struct {
+	/********** Servlet Options *************/
+	uint32_t     raw_input:1;      /*!< If this servlet consumes the raw input */
+	uint32_t     inverse_match:1;  /*!< Inverse matching, let all string that doesn't match pass */
+	uint32_t     full_match:1;     /*!< Performe full-line match */
+	const char*  eol_marker;       /*!< The end-of-line marker, by default is \n */
+	re_t*        regex;            /*!< The regular expression object */
+	
+	/********** Pipe definitions *************/
+	pipe_t       input;            /*!< The input pipe */
+	pipe_t       output;           /*!< The output pipe */
+
+	/********** Servlet Type Traits **********/
+	pstd_type_accessor_t  in_tok;  /*!< The accessor to access the token of input string */
+	pstd_type_accessor_t  out_tok; /*!< The accessor to access the token of output string */
+	pstd_type_model_t*    model;   /*!< The type model for this servlet */
+} ctx_t;
+
+static int _option_callback(pstd_option_data_t data)
+{
+	ctx_t* ctx = (ctx_t*)data.cb_data;
+	switch(data.current_option->short_opt)
+	{
+		case 'R':
+			ctx->raw_input = 1;
+			break;
+		case 'I':
+			ctx->inverse_match = 1;
+			break;
+		case 'F':
+			ctx->full_match = 1;
+			break;
+		case 'D':
+			ctx->eol_marker = data.param_array[0].strval;
+			break;
+		default:
+			ERROR_RETURN_LOG(int, "Invalid command line options");
+	}
+	return 0;
+}
+
+static int _init(uint32_t argc, char const* const* argv, void* ctxmem)
+{
+	ctx_t* ctx = (ctx_t*)ctxmem;
+
+	ctx->raw_input = 0;
+	ctx->inverse_match = 0;
+	ctx->eol_marker = "\n";
+	ctx->full_match = 0;
+	ctx->model = NULL;
+	ctx->regex = NULL;
+
+	static pstd_option_t options[] = {
+		{
+			.long_opt    = "help",
+			.short_opt   = 'h',
+			.pattern     = "",
+			.description = "Show this help message",
+			.handler     = pstd_option_handler_print_help,
+			.args        = NULL
+		},
+		{
+			.long_opt    = "raw-input",
+			.short_opt   = 'R',
+			.pattern     = "",
+			.description = "Read from the untyped input pipe instead of string pipe",
+			.handler     = _option_callback,
+			.args        = NULL
+		},
+		{
+			.long_opt    = "inverse",
+			.short_opt   = 'I',
+			.pattern     = "",
+			.description = "Do inverse match, filter all the matched string out",
+			.handler     = _option_callback,
+			.args        = NULL
+		},
+		{
+			.long_opt    = "full",
+			.short_opt   = 'F',
+			.pattern     = "",
+			.description = "Turn on the full-line-matching mode",
+			.handler     = _option_callback,
+			.args        = NULL
+		},
+		{
+			.long_opt    = "deliminator",
+			.short_opt   = 'D',
+			.pattern     = "S",
+			.description = "Set the end-of-line marker",
+			.handler     = _option_callback,
+			.args        = NULL
+		}
+	};
+
+	if(ERROR_CODE(int) == pstd_option_sort(options, sizeof(options) / sizeof(options[0])))
+		ERROR_RETURN_LOG(int, "Cannot sort the servlet option template");
+
+	uint32_t next_opt;
+	if(ERROR_CODE(uint32_t) == (next_opt = pstd_option_parse(options, sizeof(options) / sizeof(options[0]), argc, argv, ctx)))
+		ERROR_RETURN_LOG(int, "Cannot parse the command line arguments");
+
+	if(next_opt >= argc)
+		ERROR_RETURN_LOG(int, "Missing regular expression");
+
+	if(next_opt < argc - 1)
+		ERROR_RETURN_LOG(int, "Too many regular expressions");
+
+	if(ERROR_CODE(pipe_t) == (ctx->input = pipe_define("input", PIPE_INPUT, ctx->raw_input ? NULL : "plumber/std/request_local/String")))
+		ERROR_RETURN_LOG(int, "Cannot define the input pipe");
+
+	if(ERROR_CODE(pipe_t) == (ctx->output = pipe_define("output", PIPE_OUTPUT, "plumber/std/request_local/String")))
+		ERROR_RETURN_LOG(int, "Cannot define the output pipe");
+
+	if(NULL == (ctx->model = pstd_type_model_new()))
+		ERROR_RETURN_LOG(int, "Cannot create new type model");
+
+	if(ctx->raw_input == 0 && ERROR_CODE(pstd_type_accessor_t) == (ctx->in_tok = pstd_type_model_get_accessor(ctx->model, ctx->input, "token")))
+		ERROR_RETURN_LOG(int, "Cannot get the type accessor for input.token");
+
+	if(ERROR_CODE(pstd_type_accessor_t) == (ctx->out_tok = pstd_type_model_get_accessor(ctx->model, ctx->output, "token")))
+		ERROR_RETURN_LOG(int, "Cannot get the type accessor for output.token");
+
+	if(NULL == (ctx->regex = re_new(argv[next_opt])))
+		ERROR_RETURN_LOG(int, "Cannot compile the regular expression");
+
+	LOG_DEBUG("Regex servlet has been initialized successfully");
+
+	return 0;
+}
+
+static int _cleanup(void* ctxmem)
+{
+	int rc = 0;
+	ctx_t* ctx = (ctx_t*)ctxmem;
+
+	if(NULL != ctx->model && ERROR_CODE(int) == pstd_type_model_free(ctx->model))
+		rc = ERROR_CODE(int);
+	
+	if(NULL != ctx->regex && ERROR_CODE(int) == re_free(ctx->regex))
+		rc = ERROR_CODE(int);
+
+	return rc;
+}
+
 SERVLET_DEF = {
-	.desc = "The regular expression filter servelt",
-	.version = 0x0
+	.desc    = "The regular expression filter servelt",
+	.version = 0x0,
+	.size    = sizeof(ctx_t),
+	.init    = _init,
+	.unload  = _cleanup
 };
