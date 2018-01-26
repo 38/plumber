07f33524e9935bf980aa02b27ac3a445d682019e
Hao Hou <ghost89413@gmail.com>
2018-01-26 16:28:11 -0700

    We need to optimize the dispatcher before move on

--- src/module/file/module.c
+++ src/module/file/module.c
@@ -25,15 +25,26 @@ typedef enum {
 	_WRITE_MODE_CREATE   /*!< This only allows the file name that currently not exist, otherwise an error will be raised */
 } _write_mode_t;
 
+/**
+ * @todo Support the variant-sized block file, but we need to desgin a way to describe the VS_BLK structure. 
+ *       Another thing is the VS_BLK mode shouldnt support multi-region reading since it can not detect the 
+ *       record begining 
+ **/
 typedef enum {
 	_READ_MODE_LINE,          /*!< A line based text file */
 	_READ_MODE_FS_BLK,        /*!< A fixed sized block */
 	_READ_MODE_VS_BLK         /*!< A variant size block (TODO: how to determine the size? since different file might do this differently)*/
 } _read_mode_t;
 
+/**
+ * @brief an mapped memory region
+ **/
 typedef struct {
-	void* begin;
-	void* end;
+	uint32_t  inuse:1;   /*!< Indicates if current region is using by application layer code */
+	uint32_t  last:1;    /*!< Indicates this is the last region of the file */
+	char*     current;   /*!< The current read pointer */
+	char*     begin;     /*!< The beging of the region */
+	char*     end;       /*!< The end of the region */
 } _mem_region_t;
 
 /**
@@ -42,9 +53,20 @@ typedef struct {
 typedef struct {
 	char*          path;      /*!< The input path */
 	int            fd;        /*!< The input file file descriptor */
-	off_t          size;      /*!< The size of the input file */
+	size_t         size;      /*!< The size of the input file */
 	_mem_region_t* regions;   /*!< Each of the access regions */
 } _input_file_t;
+
+/**
+ * @brief Indicates that a parallel read sequence that we can read currently
+ **/
+typedef struct {
+	uint32_t is_mmap_region:1;   /*!< Indicates if this is a mmaped region */
+	union {
+		_input_file_t* file;     /*!< The file that doesn't support file */
+		_mem_region_t* region;   /*!< The mmaped region that can be read */
+	};
+} _input_sequence_t;
 
 /**
  * @brief The module isntance context
@@ -76,7 +98,14 @@ typedef struct {
 	_write_mode_t write_mode;   /*!< Which mode we want to use for the output file */
 
 	/************** Module state *********************/
+	uint32_t  input_init:1; /*!< Indicates all the inputs has initialized */
 	uint32_t  exhuasted:1;  /*!< Indicates if this module have no events */
+
+	_input_sequence_t* avail_list; /*!< The avaiable list */
+	uint32_t avail_list_begin;     /*!< The avaiable list begin */
+	uint32_t avail_list_end;       /*!< The aviable list end */
+	uint32_t avail_list_size;      /*!< The size of the availe list */
+
 
 	union {
 		struct {
@@ -92,9 +121,10 @@ typedef struct {
  * @brief The pipe handle data structure
  **/
 typedef struct {
-	void*   buf;       /*!< The current line buffer */
-	size_t  buf_size;  /*!< The size of this line buffer */
-	size_t  offset;    /*!< Current read offset */
+	uint32_t  is_input:1;/*!< If this handle is the input end of the pipe */
+	void*     buf;       /*!< The current line buffer */
+	size_t    buf_size;  /*!< The size of this line buffer (-1 for the buffer we don't know the size) */
+	size_t    offset;    /*!< Current read offset */
 } handle_t;
 
 static size_t _pagesize = 0;
@@ -111,6 +141,7 @@ static int _init(void* __restrict ctxbuf, uint32_t argc, char const* __restrict 
 	memset(ctx, 0, sizeof(context_t));
 	ctx->out_fd = -1;
 	ctx->line_param.delim = '\n';
+	ctx->parallel_num = 1;
 
 	const char* arguments[3]  = {};
 
@@ -136,10 +167,6 @@ static int _init(void* __restrict ctxbuf, uint32_t argc, char const* __restrict 
 			ERROR_RETURN_LOG(int, "Missing argument string %s, expected: file input=<input-file> output=<output-file> label=<label>", names[i]);
 		else 
 			LOG_DEBUG("Parsed argument string %s%s", names[i], arguments[i]);
-#if 0
-	if(NULL == (ctx->in_path = strdup(arguments[0])))
-		ERROR_RETURN_LOG_ERRNO(int, "Cannot duplicate the input path");
-#endif
 	j = 0;
 	for(i = 0;; i ++)
 	{
@@ -203,6 +230,9 @@ static inline int _cleanup(void* __restrict ctxbuf)
 				uint32_t j;
 				for(j = 0; j < ctx->parallel_num; j ++)
 				{
+					if(NULL == ctx->inputs[i].regions[j].begin) 
+						continue;
+
 					size_t region_size = _ROUND_PAGE((size_t)((char*)ctx->inputs[i].regions[j].end - (char*)ctx->inputs[i].regions[j].begin)); 
 
 					if(region_size > 0 && munmap(ctx->inputs[i].regions[j].begin, region_size) < 0)
@@ -216,6 +246,9 @@ static inline int _cleanup(void* __restrict ctxbuf)
 		}
 		free(ctx->inputs);
 	}
+
+	if(NULL != ctx->avail_list)
+		free(ctx->avail_list);
 
 	if(ctx->out_fd >= 0 && close(ctx->out_fd) < 0)
 	{
@@ -369,13 +402,142 @@ static int _set_prop(void* __restrict ctxmem, const char* sym, itc_module_proper
 	}
 	else if(value.type == ITC_MODULE_PROPERTY_TYPE_INT)
 	{
-		if(strcmp(sym, "parallel") == 0) ctx->parallel_num = (uint32_t)value.num;
+		if(strcmp(sym, "parallel") == 0 && (uint32_t)value.num > 0) ctx->parallel_num = (uint32_t)value.num;
 		else if(strcmp(sym, "output_perm") == 0) ctx->create_mode = (int)value.num;
 		else if(ctx->read_mode == _READ_MODE_FS_BLK && strcmp(sym, "fs_blk.size") == 0)
 			ctx->fs_blk_param.size = (size_t)value.num;
 		else  return 0;
 		return 1;
 	}
+
+	return 0;
+}
+
+static int _ensure_inputs(context_t* ctx)
+{
+	if(ctx->input_init) return 0;
+
+	size_t max_num_seq = ctx->num_inputs * ctx->parallel_num;
+	
+	ctx->avail_list_size = 1;
+	for(;ctx->avail_list_size < max_num_seq; ctx->avail_list_size *= 2);
+
+	if(NULL == (ctx->avail_list = (_input_sequence_t*)malloc(sizeof(_input_sequence_t) * ctx->avail_list_size)))
+		ERROR_RETURN_LOG(int, "Cannot allocate memory for the avaiable list");
+
+	ctx->avail_list_begin = ctx->avail_list_end = 0;
+
+	uint32_t i,j;
+	for(i = 0; i < ctx->num_inputs; i ++)
+	{
+		if((ctx->inputs[i].fd = open(ctx->inputs[i].path, O_RDONLY)) < 0)
+		{
+			LOG_WARNING_ERRNO("Unable to open file %s", ctx->inputs[i].path);
+			continue;
+		}
+
+		struct stat stbuf;
+		if(fstat(ctx->inputs[i].fd, &stbuf) < 0)
+		{
+			LOG_NOTICE_ERRNO("Unable to stat the file to get the file size, do not use mmap IO");
+			goto STREAM_IO;
+		}
+
+		size_t region_size = _ROUND_PAGE((size_t)stbuf.st_size);
+
+		ctx->inputs[i].size = (size_t)stbuf.st_size;
+
+		if(NULL == (ctx->inputs[i].regions = (_mem_region_t*)calloc(ctx->parallel_num, sizeof(_mem_region_t))))
+		{
+			LOG_WARNING_ERRNO("Cannot allocate memory for the region");
+			goto STREAM_IO;
+		}
+
+		if(region_size > 0)
+		{
+			char* region = (char*)mmap(NULL, region_size, PROT_READ, MAP_PRIVATE, ctx->inputs[i].fd, 0);
+			if(NULL == region)
+			{
+				LOG_NOTICE_ERRNO("Unable to mmap the file into address space, try to use normal IO");
+				goto STREAM_IO;
+			}
+			else LOG_INFO("Mapped memory region: %s => [%p, %p)", ctx->inputs[i].path, region, region + ctx->inputs[i].size);
+
+			size_t block_size = (ctx->inputs[i].size + ctx->parallel_num - 1) / ctx->parallel_num;
+			size_t block_offset = 0;
+
+			for(j = 0; j < ctx->parallel_num; j ++)
+			{
+				if(block_offset >= ctx->inputs[i].size) 
+				{
+					ctx->inputs[i].regions[j].begin = ctx->inputs[i].regions[j].end = NULL;
+					continue;
+				}
+
+				size_t desired_end = block_offset + block_size;
+				size_t actual_end = _ROUND_PAGE(desired_end);
+
+				if(actual_end > ctx->inputs[i].size) 
+				{
+					actual_end = ctx->inputs[i].size;
+					ctx->inputs[i].regions[j].last = 1;
+				}
+
+				ctx->inputs[i].regions[j].begin = region + block_offset;
+				ctx->inputs[i].regions[j].end   = region + actual_end;
+				ctx->inputs[i].regions[j].current = begin;
+
+				if(ctx->read_mode == _READ_MODE_LINE)
+				{
+					/* In this case, we should make current location after the first '\n' */
+
+					if(j > 0 && ctx->inputs[i].regions[j - 1].end[-1] != ctx->eol_marker)
+					{
+						char* next = (char*)memchr(ctx->inputs[i].regions[j].begin, (char)ctx->eol_marker, ctx->inputs[i].regions[j].end - ctx->inputs[i].regions[j].begin);
+						if(NULL == next)
+						{
+							/* This indicates that the entire region belongs to previous line */
+						}
+						else ctx->inputs[i].regions[j].current = next + 1;
+					}
+				}
+
+				LOG_INFO("Memory map region %u: [%p, %p) file = %s", j, ctx->inputs[i].regions[j].begin, ctx->inputs[i].regions[j].end, ctx->inputs[i].path);
+
+				block_offset = actual_end;
+
+				ctx->avail_list[ctx->avail_list_end].is_mmap_region = 1;
+				ctx->avail_list[ctx->avail_list_end].region = &ctx->inputs[i].regions[j];
+				ctx->avail_list_end ++;
+			}
+		}
+		continue;
+STREAM_IO:
+		ctx->avail_list[ctx->avail_list_end].is_mmap_region = 0;
+		ctx->avail_list[ctx->avail_list_end].file = &ctx->inputs[i];
+		ctx->avail_list_end ++;
+	}
+
+	return 0;
+}
+
+static int _accept(void* __restrict ctxmem, const void* __restrict args, void* __restrict inbuf, void* __restrict outbuf)
+{
+	(void)args;
+	context_t* ctx = (context_t*)ctxmem;
+
+	if(ERROR_CODE(int) == _ensure_inputs(ctx))
+		ERROR_RETURN_LOG(int, "Cannot ensure all the input file are initialized");
+
+	handle_t* in = (handle_t*)inbuf;
+	handle_t* out = (handle_t*)outbuf;
+
+	in->is_input = 1;
+	in->buf = NULL;
+	in->buf_size = (size_t)-1;
+	in->offset = 0;
+
+	out->is_input = 0;
 
 	return 0;
 }
@@ -389,5 +551,6 @@ itc_module_t module_file_module_def = {
 	.get_path       = _get_path,
 	.get_flags      = _get_flags,
 	.get_property   = _get_prop,
-	.set_property   = _set_prop
+	.set_property   = _set_prop,
+	.accept         = _accept
 };
