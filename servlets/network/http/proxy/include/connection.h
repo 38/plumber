daa9df4e6694b9f41368cbdd4b5fe7ea6cb2e4ef
Hao Hou <ghost89413@gmail.com>
2018-02-10 14:58:35 -0700

    Add headers for the servlet

---
+++ servlets/network/http/proxy/include/connection.h
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2018, Hao Hou
+ **/
+/**
+ * @brief The connection management utilities for the proxy servlet
+ * @file proxy/include/connection.h
+ **/
+#ifndef __CONNECTION_H__
+#define __CONNECTION_H__
+
+/**
+ * @brief The connection pool initializaiont function (Called from each servlet)
+ * @note The pool is a singleton shared between all the workers and servlets
+ * @param size How many connections the pool can hold
+ * @param peer_pool_size How many connections can be hold to the same peer
+ * @return status code
+ **/
+int connection_pool_init(uint32_t size, uint32_t peer_pool_size);
+
+/**
+ * @brief The connection pool finalization (Called from each servlet)
+ * @return status code
+ **/
+int connection_pool_finalize();
+
+/**
+ * @brief Acquire a connection from the connection pool
+ * @param hostname The destination host name
+ * @param port The destination port
+ * @return The socket FD that is connected the target
+ **/
+int connection_pool_acquire(const char* hostname, uint16_t port);
+
+/**
+ * @brief Release the connection and return it to the connection pool
+ * @param hostname The peer hostname
+ * @param port The port 
+ * @param fd The socket FD to release
+ * @return status code
+ **/
+int connection_pool_release(const char* hostname, uint16_t port, int fd);
+
+#endif
---
+++ servlets/network/http/proxy/include/request.h
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2017, Hao Hou
+ **/
+/**
+ * @brief The RLS object for a proxy HTTP request
+ * @file proxy/include/request.h
+ **/
+
+#ifndef __REQUEST_H__
+#define __REEUEST_H__
+/**
+ * @brief The method of a HTTP request
+ **/
+typedef enum {
+	REQUEST_METHOD_GET,    /*!< The HTTP GET method */
+	REQUEST_METHOD_POST,   /*!< The HTTP POST method */
+	REQUEST_METHOD_HEAD,   /*!< The HTTP HEAD method */
+	REQUEST_METHOD_PUT,    /*!< The HTTP PUT method */
+	REQUEST_METHOD_DELETE  /*!< The HTTP DELETE method */
+} request_method_t;
+
+/**
+ * @brief The declaration for a RLS proxy object
+ **/
+typedef struct _request_t request_t;
+
+/**
+ * @brief Create a new request RLS proxy
+ * @param method The request method
+ * @param url The url for the request
+ * @param data The addtional payload data
+ * @return status code
+ **/
+request_t* request_new(request_method_t method, const char* url, const char* data);
+
+/**
+ * @brief Commit the request to the RLS scope
+ * @param request The request object 
+ * @return The token that is used to refer the object
+ **/
+scope_token_t request_commit(request_t* request);
+
+#endif
