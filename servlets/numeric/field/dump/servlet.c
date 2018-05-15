0bd258bd0902d26311c84d78541f457c66c58d5d
Hao Hou <ghost89413@gmail.com>
2018-05-15 14:59:06 -0600

    Add a servlet so that we can dump the field result to raw pipe

---
+++ servlets/numeric/field/dump/servlet.c
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2018, Hao Hou
+ **/
+#include <pservlet.h>
+#include <pstd.h>
+
+#include <psnl.h>
+
+SERVLET_DEF = {
+	.desc = "Dump the field to either raw pipe or a RLS string object",
+	.version = 0x0,
+	.size = 0
+};
