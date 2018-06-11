3d7493669fc75bd0cc6fd6fd32fde4a59d090c6a
Hao Hou <ghost89413@gmail.com>
2018-06-11 09:44:02 -0600

    Still working on the expression representation

--- lib/psnl/include/psnl/expr.h
+++ lib/psnl/include/psnl/expr.h
@@ -25,15 +25,18 @@ typedef enum {
  * @brief Indicates what operation we should performe
  **/
 typedef enum {
-	PSNL_EXPR_OP_MAT_MUL    /*!< Performe the matrix multiplication */
+	PSNL_EXPR_OP_ERROR = ERROR_CODE(int),   /*!< The opcode indicates an error */
+	PSNL_EXPR_OP_ADD,                       /*!< Add a field with something */
+	PSNL_EXPR_OP_COUNT                      /*!< The number of the opcode */
 } psnl_expr_op_t;
 
 /**
  * @brief The type of a expression  operand
  **/
 typedef enum {
-	PSNL_EXPR_OPERAND_TYPE_EXPR,   /*!< If the operand is another expression */
-	PSNL_EXPR_OPERAND_TYPE_FIELD   /*!< If the operand is a field */
+	PSNL_EXPR_OPERAND_TYPE_EXPR,   /*!< another expression as an opeerand */
+	PSNL_EXPR_OPERAND_TYPE_FIELD,  /*!< a field as an operand */
+	PSNL_EXPR_OPERAND_TYPE_SCALAR  /*!< a scalar value as an operand */
 } psnl_expr_operand_type_t;
 
 /**
@@ -45,6 +48,7 @@ typedef struct {
 		psnl_cpu_field_t*    cpu_field; /*!< The CPU field */
 		/* TODO: Add GPU field support */
 		psnl_expr_t*         expr;      /*!< The operand is another expression */
+		double               scalar;    /*!< The scalar value */
 	}                        data;      /*!< The actual data type */
 } psnl_expr_operand_t;
 
@@ -65,9 +69,10 @@ typedef int (*psnl_expr_exec_at_cb_t)(const psnl_expr_operand_t* oper, int32_t* 
  * @param res_elem_size The result element size in bytes
  * @param cpu_exec If given it means this conintuation can be run on CPU directly
  * @param range The valid range of this continuation
+ * @param data The addtional data for this expression
  * @return the newly created expression
  **/
-psnl_expr_t* psnl_expr_new(psnl_expr_op_t op, psnl_expr_operand_t* oper, const psnl_dim_t* range, size_t res_elem_size, psnl_expr_exec_at_cb_t cpu_exec);
+psnl_expr_t* psnl_expr_new(psnl_expr_op_t op, psnl_expr_operand_t* oper, const psnl_dim_t* range, size_t res_elem_size, psnl_expr_exec_at_cb_t cpu_exec, void* data);
 
 /**
  * @brief Dispose a use expression
@@ -105,9 +110,17 @@ int psnl_expr_decref(const psnl_expr_t* expr);
  **/
 int psnl_expr_compute(const psnl_expr_t* expr, void* buffer);
 
-#if 0
-//TODO: desgin the API
-int psnl_expr_traverse(const psnl_expr_t* expr, 
-#endif
+/**
+ * @brief Get the opcode of the expression
+ * @param expr The expression object
+ * @return The opcode of the expression or error code
+ **/
+psnl_expr_op_t psnl_expr_get_opcode(const psnl_expr_t* expr);
+
+const psnl_dim_t* psnl_expr_get_range(const psnl_expr_t* expr);
+
+size_t psnl_expr_get_element_size(const psnl_expr_t* expr);
+
+const void* psnl_expr_get_data(const psnl_expr_t* expr);
 
 #endif /* __PSNL_EXPR_H__ */
